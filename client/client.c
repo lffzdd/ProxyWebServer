75c4a741d05315bb2f5e2990da193418d72b1c04
lff2060wsl2 <c1471373444@outlook.com>
2025-06-23 13:29:08 +0800

    重构客户端代码以实现基于epoll的事件驱动模型

--- client/client.c
+++ client/client.c
@@ -6,26 +6,41 @@
 #include "config.h"
 #include "net_utils.h"
 
-#include <openssl/err.h>
-#include <openssl/ssl.h>
+#include<sys/epoll.h>
 
 int main(int argc, char const* argv[]) {
 
     int listen_fd = openListenfd(PORT);
     listen(listen_fd, 5);
 
+    int epfd = epoll_create1(0);
+
+    struct epoll_event events[1024];
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = listen_fd;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, listen_fd, &ev);
+
     while (1) {
-        int client_fd = acceptClientfd(listen_fd);
+        int nready = epoll_wait(epfd, events, 1024, -1);
+        for (int i = 0; i < nready; i++) {
+            int fd = events[i].data.fd;
+            if (fd == listen_fd) {
+                int client_fd = acceptClientfd(listen_fd);
 
-        if (PROXY_MODE) {
-            proxyClient(client_fd);
-        }
-        else {
-            respondClient(client_fd);
+                ev.events = EPOLLIN;
+                ev.data.fd = client_fd;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, client_fd, &ev);
+            }
+            else {
+                respondClient(fd);
+                epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+                close(fd);
+            }
         }
 
-        close(listen_fd);
     }
+    close(listen_fd);
 
     return 0;
 }
